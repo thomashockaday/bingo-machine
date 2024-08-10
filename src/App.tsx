a8241370a5d0abb134309ad99c5a3a5d148beaf2
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2024-08-11 00:53:50 +0100

    Allow numbers to be highlighted

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 function App() {
-  const numbers = [];
+  const [calledNumbers] = useState<number[]>([10]);
+
+  const numbers: number[] = [];
 
   for (let i = 1; i <= 90; i += 1) {
     numbers.push(i);
@@ -9,7 +13,13 @@ function App() {
     <div className="bingo-machine">
       <div className="bingo-board">
         {numbers.map((i) => (
-          <div className="bingo-board-cell">{i}</div>
+          <div
+            className={`bingo-board-cell ${
+              calledNumbers.includes(i) ? "bingo-board-cell--called" : ""
+            }`}
+          >
+            {i}
+          </div>
         ))}
       </div>
     </div>
