772ac684b50dfb5c04d10060843d8ae5bbb5ab65
Thomas Hockaday <thomashockaday@intelligencefusion.co.uk>
2024-08-11 01:07:39 +0100

    Add a button to call a number

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [calledNumbers] = useState<number[]>([10]);
+  const [calledNumbers, setCalledNumbers] = useState<number[]>([]);
 
   const numbers: number[] = [];
 
   for (let i = 1; i <= 90; i += 1) {
     numbers.push(i);
+  }
+
+  function callNextNumber(): void {
+    const random = Math.floor(Math.random() * 90) + 1;
+
+    if (calledNumbers.includes(random)) {
+      callNextNumber();
+    } else {
+      setCalledNumbers([...calledNumbers, random]);
+    }
   }
 
   return (
@@ -17,11 +27,14 @@ function App() {
             className={`bingo-board-cell ${
               calledNumbers.includes(i) ? "bingo-board-cell--called" : ""
             }`}
+            key={i}
           >
             {i}
           </div>
         ))}
       </div>
+
+      <button onClick={callNextNumber}>Call</button>
     </div>
   );
 }
